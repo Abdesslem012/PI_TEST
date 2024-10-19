c227a819f438e8ba9989f22711f5103abacbba41
Abdesslem012 <146556374+Abdesslem012@users.noreply.github.com>
2024-10-19 21:05:34 +0100

    Update SecurityConfig.java

--- demo1/src/main/java/com/example/demo/Security/SecurityConfig.java
+++ demo1/src/main/java/com/example/demo/Security/SecurityConfig.java
@@ -1,15 +1,15 @@
 package com.example.demo.Security;
 
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
+
 
 import java.util.Collections;
 import java.util.List;
@@ -18,32 +18,21 @@ import java.util.List;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Autowired
-    KeycloakRoleConverter keycloakRoleConverter;
-
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.sessionManagement(session ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors
-                        .configurationSource(new CorsConfigurationSource() {
-                            @Override
-                            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                                CorsConfiguration cors = new CorsConfiguration();
-                                cors.setAllowedOrigins(List.of("http://localhost:4200"));
-                                cors.setAllowedMethods(Collections.singletonList("*"));
-                                cors.setAllowedHeaders(Collections.singletonList("*"));
-                                cors.setExposedHeaders(Collections.singletonList("Authorization"));
-                                return cors;
-                            }
-                        }))
-                 .authorizeRequests(authorizeRequests ->
-                 authorizeRequests
-
-                      .anyRequest().permitAll())
-                .oauth2ResourceServer(rs -> rs.jwt(jwt ->
-                        jwt.jwtAuthenticationConverter(keycloakRoleConverter)));
+    public SecurityFilterChain filterChain(HttpSecurity http, KeycloakRoleConverter keycloakRoleConverter) throws Exception {
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(request -> {
+                    CorsConfiguration cors1 = new CorsConfiguration();
+                    cors1.setAllowedOrigins(List.of("http://localhost:4200"));
+                    cors1.setAllowedMethods(Collections.singletonList("*"));
+                    cors1.setAllowedHeaders(Collections.singletonList("*"));
+                    cors1.setExposedHeaders(Collections.singletonList("Authorization"));
+                    return cors1;
+                }))
+                .authorizeRequests(authorizeRequests -> authorizeRequests
+                        .anyRequest().permitAll())
+                .oauth2ResourceServer(rs -> rs.jwt(jwt -> jwt.jwtAuthenticationConverter(keycloakRoleConverter)));
 
         return http.build();
     }
